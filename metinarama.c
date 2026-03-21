e9033c08c0547dc0ee2e3d11f1e3e558d1ef212b
AngeloNero <alperentuna2007@gmail.com>
2026-03-21 23:31:12 +0300

    C versiyonu eklendi

--- metinarama.c
+++ metinarama.c
@@ -3,121 +3,177 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+#define MAX_SATIR 500
 #define MAX_KELIME 100
-#define MAX_DOSYA_ADI 100
-#define MAX_SATIR 1000
 
-void kucukHarfeCevir(char *yazi)
-{
-	int i;
-    for (i = 0; yazi[i] != '\0'; i++)
-    {
-        yazi[i] = tolower((unsigned char)yazi[i]);
-    }
-}
-
-int satirdaGecisSayisi(const char *satir, const char *hedef)
-{
-    int sayac = 0;
-    const char *ptr = satir;
-    int hedefUzunluk = strlen(hedef);
-
-    while ((ptr = strstr(ptr, hedef)) != NULL)
-    {
-        sayac++;
-        ptr += hedefUzunluk;
-    }
-
-    return sayac;
-}
+int bos_mu(char metin[]);
+int esit_mi_buyuk_kucuk_harf_duyarsiz(char a[], char b[]);
+int satirda_kelime_sayisi(char satir[], char kelime[]);
 
 int main()
 {
-    char hedef_kelime[MAX_KELIME];
-    char dosya_adi[MAX_DOSYA_ADI];
+    FILE *girisDosyasi;
+    FILE *cikisDosyasi;
+
+    char girisDosyaAdi[] = "metin.txt";
+    char cikisDosyaAdi[] = "sonuc.txt";
+    char videoYolu[] = "video.mp4";
+
+    char kelime[MAX_KELIME];
     char satir[MAX_SATIR];
-    char satirKopya[MAX_SATIR];
 
     int satirNo = 0;
-    int toplamTekrar = 0;
+    int toplamAdet = 0;
     int gecenSatirSayisi = 0;
+    int satirdakiAdet;
 
-    printf("Aramak istediginiz kelime veya cumleyi giriniz: ");
-    fgets(hedef_kelime, sizeof(hedef_kelime), stdin);
-    hedef_kelime[strcspn(hedef_kelime, "\n")] = '\0';
+    girisDosyasi = fopen(girisDosyaAdi, "r");
 
-    if (strlen(hedef_kelime) == 0)
+    if (girisDosyasi == NULL)
     {
-        printf("Bos giris yapildi!\n");
+        printf("metin.txt dosyasi bulunamadi!\n");
         return 1;
     }
 
-    if (strcmp(hedef_kelime, "let it happen") == 0)
+    printf("Aranacak kelimeyi girin: ");
+    fgets(kelime, MAX_KELIME, stdin);
+
+    kelime[strcspn(kelime, "\n")] = '\0';
+
+    if (bos_mu(kelime))
     {
-        system("start video.mp4");
+        printf("Gecerli bir giris yapmadiniz!\n");
+        fclose(girisDosyasi);
+        return 1;
+    }
+
+    if (esit_mi_buyuk_kucuk_harf_duyarsiz(kelime, "let it happen"))
+    {
+        FILE *videoKontrol = fopen(videoYolu, "r");
+
+        if (videoKontrol != NULL)
+        {
+            fclose(videoKontrol);
+            system("start video.mp4");
+            printf("Video acildi.\n");
+        }
+        else
+        {
+            printf("video.mp4 dosyasi bulunamadi!\n");
+        }
+
+        fclose(girisDosyasi);
         return 0;
     }
 
-    printf("Hangi dosyada ariyorsunuz? ");
-    scanf("%99s", dosya_adi);
+    cikisDosyasi = fopen(cikisDosyaAdi, "w");
 
-    FILE *file = fopen(dosya_adi, "r");
-    if (file == NULL)
+    if (cikisDosyasi == NULL)
     {
-        printf("Hata: Dosya bulunamadi veya acilamadi!\n");
+        printf("sonuc.txt dosyasi olusturulamadi!\n");
+        fclose(girisDosyasi);
         return 1;
     }
 
-    FILE *rapor = fopen("rapor.txt", "w");
-    if (rapor == NULL)
-    {
-        printf("Hata: rapor.txt olusturulamadi!\n");
-        fclose(file);
-        return 1;
-    }
+    fprintf(cikisDosyasi, "=== METIN ARAMA RAPORU ===\n");
+    fprintf(cikisDosyasi, "Aranan kelime: %s\n\n", kelime);
 
-    char hedefKopya[MAX_KELIME];
-    strcpy(hedefKopya, hedef_kelime);
-    kucukHarfeCevir(hedefKopya);
-
-    fprintf(rapor, "=== METIN ARAMA RAPORU ===\n");
-    fprintf(rapor, "Aranan ifade: %s\n\n", hedef_kelime);
-
-    while (fgets(satir, sizeof(satir), file) != NULL)
+    while (fgets(satir, MAX_SATIR, girisDosyasi) != NULL)
     {
         satirNo++;
+        satirdakiAdet = satirda_kelime_sayisi(satir, kelime);
 
-        strcpy(satirKopya, satir);
-        kucukHarfeCevir(satirKopya);
-
-        int adet = satirdaGecisSayisi(satirKopya, hedefKopya);
-
-        if (adet > 0)
+        if (satirdakiAdet > 0)
         {
             gecenSatirSayisi++;
-            toplamTekrar += adet;
+            toplamAdet += satirdakiAdet;
 
-            fprintf(rapor, "%d. satirda %d kez bulundu.\n", satirNo, adet);
-            fprintf(rapor, "Satir: %s", satir);
+            fprintf(cikisDosyasi, "%d. satirda %d kez bulundu.\n", satirNo, satirdakiAdet);
+            fprintf(cikisDosyasi, "Satir: %s", satir);
 
             if (satir[strlen(satir) - 1] != '\n')
             {
-                fprintf(rapor, "\n");
+                fprintf(cikisDosyasi, "\n");
             }
 
-            fprintf(rapor, "\n");
+            fprintf(cikisDosyasi, "\n");
         }
     }
 
-    fprintf(rapor, "=== OZET ===\n");
-    fprintf(rapor, "Ifadenin gectigi satir sayisi: %d\n", gecenSatirSayisi);
-    fprintf(rapor, "Ifadenin toplam tekrar sayisi: %d\n", toplamTekrar);
+    fprintf(cikisDosyasi, "=== OZET ===\n");
+    fprintf(cikisDosyasi, "Kelimenin gectigi satir sayisi: %d\n", gecenSatirSayisi);
+    fprintf(cikisDosyasi, "Kelimenin toplam gecis sayisi: %d\n", toplamAdet);
 
-    fclose(file);
-    fclose(rapor);
+    fclose(girisDosyasi);
+    fclose(cikisDosyasi);
 
     printf("Arama tamamlandi.\n");
-    printf("Sonuclar rapor.txt dosyasina yazildi.\n");
+    printf("Sonuclar sonuc.txt dosyasina yazildi.\n");
 
     return 0;
+}
+
+int bos_mu(char metin[])
+{
+    int i;
+
+    for (i = 0; metin[i] != '\0'; i++)
+    {
+        if (metin[i] != ' ' && metin[i] != '\t')
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int esit_mi_buyuk_kucuk_harf_duyarsiz(char a[], char b[])
+{
+    int i = 0;
+
+    while (a[i] != '\0' && b[i] != '\0')
+    {
+        if (tolower(a[i]) != tolower(b[i]))
+        {
+            return 0;
+        }
+        i++;
+    }
+
+    if (a[i] == '\0' && b[i] == '\0')
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int satirda_kelime_sayisi(char satir[], char kelime[])
+{
+    int i, j;
+    int sayac = 0;
+    int bulundu;
+
+    for (i = 0; satir[i] != '\0'; i++)
+    {
+        bulundu = 1;
+
+        for (j = 0; kelime[j] != '\0'; j++)
+        {
+            if (tolower(satir[i + j]) != tolower(kelime[j]))
+            {
+                bulundu = 0;
+                break;
+            }
+        }
+
+        if (bulundu == 1 && kelime[0] != '\0')
+        {
+            sayac++;
+            i = i + j - 1;
+        }
+    }
+
+    return sayac;
 }
